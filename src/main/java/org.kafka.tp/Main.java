7b0b7ec9b4aafcd702a07a42c7fc04fcd17792dc
NekoRomain <romainwangon@gmail.com>
2020-11-16 14:20:36 +0100

    First commit

---
+++ src/main/java/org.kafka.tp/Main.java
@@ -0,0 +1,13 @@
+package org.kafka.tp;
+
+public class Main{
+        public Main() throws InterruptedException {
+            while (true){
+
+            }
+        }
+
+        public static void main(String[] args) throws InterruptedException {
+            Main main = new Main();
+        }
+}
